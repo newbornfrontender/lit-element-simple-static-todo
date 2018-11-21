e327fca6a0f3e34fb314574ed4120a564c618050
Infant Frontender <newbornfrontender@gmail.com>
2018-11-21 12:48:32 +0300

    Добавлен основной js файл

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { TodoList } from './components/todo-list';
+import { TodoItem } from './components/todo-item';
+
+customElements.define('todo-item', TodoItem);
+customElements.define('todo-list', TodoList);
